327f4563d83ab24c3a69c18aa643722cd43148f6
yavarmish <59411957+yavarmish@users.noreply.github.com>
2022-07-09 12:21:40 +0530

    Update connection.php

--- PHP/connection.php
+++ PHP/connection.php
@@ -1,5 +1,5 @@
 <?php
-$link=mysqli_connect("localhost","yavarmis","Yashmishra_169","yavarmis_onlinenotes");
+$link=mysqli_connect("localhost","yavarmis","password","yavarmis_onlinenotes");
 if(mysqli_connect_error())
 die("ERROR: Unable to connect:" . mysqli_connect_error());
 ?>
